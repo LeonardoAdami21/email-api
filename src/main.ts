be14d0d88e94a60322b1b5e6a43c5c2ba9245abd
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-05-01 09:53:44 -0300

    refactor/email-api, mais arquivos implementados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './config/winston.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import path, { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = WinstonModule.createLogger(winstonConfig);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { logger });
+  app.enableCors();
+  app.setBaseViewsDir(join(__dirname + '/public'));
+  app.setViewEngine('hbs');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Email Api')
+    .setDescription('Tecnologias: Nestjs, Swagger, TypeOrm, Postgres e Docker')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(process.env.APP_PORT || 3000, () =>
+    console.log(
+      `App is Running\nDocumentation available on http://localhost:${process.env.APP_PORT}/docs`,
+    ),
+  );
 }
 bootstrap();
